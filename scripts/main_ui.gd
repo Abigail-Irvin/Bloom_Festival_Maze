887b1ac52e90e52e1a06736a59d4a72ed4712a11
Abigail <walterdirvin@gmail.com>
2025-04-07 21:03:57 -0400

    feat: adding in pause menu.

--- scripts/main_ui.gd
+++ scripts/main_ui.gd
@@ -2,7 +2,22 @@ extends Control
 @export var firefly_text_ref: RichTextLabel = null 
 @export var timer_text_ref: RichTextLabel = null
 @export var max_fireflies: int = 2
+@export var playerNode: Node = null
+@export var pauseMenu: Node = null
 
 func ui_update(count: int, time_left: float) -> void:
 	firefly_text_ref.text = str(count) + "/" + str(max_fireflies)
 	timer_text_ref.text = "Time left: " + str("%0.2f" % time_left, " s")
+
+
+func _on_return_pressed() -> void:
+	if playerNode:
+		playerNode.toggle_pause()
+
+
+func _on_quit_pressed() -> void:
+	if playerNode:
+		playerNode.exit_game()
+
+func toggle_pause_menu(paused: bool):
+	pauseMenu.set_visible(paused)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,15 +1,29 @@
+# Author: Abigail Irvin
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED: float = 300.0
+var win_state: int = 0
+var paused: bool = false
 @export var MainUi: Control = null
 @export var firefly_count: int = 0
 @export var time_left: float = 90
 @onready var firefly_jar = preload("res://scenes/firefly_jar.tscn")
 
+func _ready() -> void:
+	MainUi.toggle_pause_menu(paused)
+
 func _physics_process(delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Description
+	# ----------
+	# Basic physics process that handles basic movement for player.
+	#
+	# Parameters
+	# ----------
+	# delta : float
+	#	represents the time between frames, used to do time-based calculations
+	if is_locked():
+		return
 	var x_axis := Input.get_axis("ui_left", "ui_right")
 	if x_axis:
 		velocity.x = x_axis * SPEED
@@ -25,12 +39,48 @@ func _physics_process(delta: float) -> void:
 	MainUi.ui_update(firefly_count, time_left)
 
 func _input(event: InputEvent) -> void:
+	# Description
+	# ----------
+	# Basic input process that handles pickup / drop commands. as well as
+	# pausing / unpausing the game.
+	#
+	# Parameters
+	# ----------
+	# event : InputEvent
+	#	input event given by user
+	if event.is_action_pressed("pause"):
+		toggle_pause()
+	if is_locked():
+		return
 	if event.is_action_pressed("drop") and firefly_count > 0:
 		firefly_count -= 1
 		var instance = firefly_jar.instantiate()
 		get_tree().root.add_child(instance)
 		instance.global_position = self.global_position
 		
-
+func is_locked() -> bool:
+	# Description
+	# ----------
+	# Returns a bool stating if the game should be considered "locked"
+	# i.e. paused or won/lost.
+	return win_state or time_left < 0.0 or paused
+	
 func add_light() -> void:
+	# Description
+	# ----------
+	# Callback function activated whenever the player picks up a light jar.
 	firefly_count += 1
+
+func win_game() -> void:
+	# Description
+	# ----------
+	# Callback function activated when player runs across the win area.
+	win_state = 1
+
+func toggle_pause() -> void:
+	if not (win_state or time_left < 0.0):
+		paused = !paused
+		MainUi.toggle_pause_menu(paused)
+		
+func exit_game() -> void:
+	print("quit! :3")
